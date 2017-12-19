5d737ffa37ec05fdd8f2a95342b8681b3114ffc2
gaperaltav <gaperaltav@gmail.com>
2017-12-18 23:26:48 -0400

    Adding prop-types and converting contactlist to stateless component

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import css from './index.css';
+import React from 'react';
+import propTypes from 'prop-types';
+import './index.css';
 
-class ContactList extends Component {
-    render() {
-        return (
-            <ol className="contact-list" >
-                {this.props.contacts.map(contact => (
-                    <li key={contact.id} className="contact-list-item"  >
-                       <div className="contact-avatar" style={{
-                           backgroundImage: `url(${contact.avatarURL})`
-                           }} />
-                       <div className="contact-details" >
-                            <p>{contact.name}</p>
-                            <p>{contact.email}</p>
-                        </div>
-                        <button onClick={()=> this.props.onDeleteContact(contact)} className="contact-remove" />
-                    </li>
-                ))}
-            </ol>
-        )
-    };
+function ContactList(props) {
+
+    return (
+        <ol className="contact-list" >
+            {props.contacts.map(contact => (
+                <li key={contact.id} className="contact-list-item"  >
+                    <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }} />
+                    <div className="contact-details" >
+                        <p>{contact.name}</p>
+                        <p>{contact.email}</p>
+                    </div>
+                    <button onClick={() => props.onDeleteContact(contact)} className="contact-remove" />
+                </li>
+            ))}
+        </ol>
+    )
 }
+
+ContactList.propTypes = {
+    contacts: propTypes.array.isRequired,
+    onDeleteContact: propTypes.func.isRequired
+};
 
 export default ContactList;
