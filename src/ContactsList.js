08dfaff118c8138975c715dad3e0a7ae0707371a
gaperaltav <gaperaltav@gmail.com>
2017-12-29 00:22:17 -0400

    Creating search bar and change status in ContactList componet

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import './index.css';
 
-function ContactList(props) {
+class ContactList extends Component {
 
-    return (
-        <ol className="contact-list" >
-            {props.contacts.map(contact => (
-                <li key={contact.id} className="contact-list-item"  >
-                    <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }} />
-                    <div className="contact-details" >
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    <button onClick={() => props.onDeleteContact(contact)} className="contact-remove" />
-                </li>
-            ))}
-        </ol>
-    )
+    static propTypes = {
+        contacts: propTypes.array.isRequired,
+        onDeleteContact: propTypes.func.isRequired
+    }
+
+    state = {
+        query: ''
+    }
+
+    changeSearch(queryText){
+        this.setState({
+            query: queryText
+        })
+    }
+
+    render() {
+        return (
+            <div className="list-contacts" >
+                <div className="list-contacts-top" >
+                <input 
+                    className="search-contacts"
+                    onChange={(event) => this.changeSearch(event.target.value)}
+                />
+                </div>
+                <ol className="contact-list" >
+                    {this.props.contacts.map(contact => (
+                        <li key={contact.id} className="contact-list-item"  >
+                            <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }} />
+                            <div className="contact-details" >
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove" />
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
+
 }
-
-ContactList.propTypes = {
-    contacts: propTypes.array.isRequired,
-    onDeleteContact: propTypes.func.isRequired
-};
 
 export default ContactList;
