e65985ac1cd64d9ca150f57ff54731faf8f309ed
gaperaltav <gaperaltav@gmail.com>
2018-01-03 23:47:16 -0400

    Adding search contact based on the query state of the controlled form

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 import './index.css';
 
 class ContactList extends Component {
@@ -13,23 +15,35 @@ class ContactList extends Component {
         query: ''
     }
 
-    changeSearch(queryText){
+    udpateQuery(queryText) {
         this.setState({
             query: queryText
         })
     }
 
     render() {
+        let showingContacts;
+        if (this.state.query) {
+            const match = new RegExp(escapeRegExp(this.state.query), 'i')    
+            showingContacts = this.props.contacts.filter( contact => match.test(contact.name))
+        } else {
+            showingContacts = this.props.contacts        
+        }
+
+        showingContacts.sort(sortBy('name'))
+
         return (
             <div className="list-contacts" >
                 <div className="list-contacts-top" >
-                <input 
-                    className="search-contacts"
-                    onChange={(event) => this.changeSearch(event.target.value)}
-                />
-                </div>
+                    <input
+                        className="search-contacts"
+                        type="text"
+                        value={this.state.query}
+                        onChange={(event) => this.udpateQuery(event.target.value)}
+                    />
+                </div>   
                 <ol className="contact-list" >
-                    {this.props.contacts.map(contact => (
+                    {showingContacts.map(contact => (
                         <li key={contact.id} className="contact-list-item"  >
                             <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }} />
                             <div className="contact-details" >
