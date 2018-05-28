9d06d4a74d89d0dc28da3edb58f73ce5c4f14c7e
gaperaltav <gaperaltav@gmail.com>
2018-05-28 09:54:51 -0400

    Fetching contacts form ContactsApi

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import React, { Component } from 'react';
 import ContactsList from './ContactsList';
-
+import * as ContactsApi from './utils/ContactsAPI'
 
 class App extends Component {
 
   state = {
-    contacts : [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
+  }
+
+  componentDidMount = () => {
+    this.initContacts()
+  }
+
+  initContacts = () => {
+    ContactsApi
+      .getAll()
+      .then(contacts => this.setState({ contacts }))
   }
 
   removeContact = (contact) => {
-    this.setState((state) => ({
-        contacts: state.contacts.filter(c => c.id !== contact.id)
-    }))
+    ContactsApi
+      .remove(contact)
+      .then(() => this.initContacts())
   }
 
   render() {
-    return(
+    return (
       <div>
         <ContactsList
-          onDeleteContact={this.removeContact}  
+          onDeleteContact={this.removeContact}
           contacts={this.state.contacts} />
       </div>
     );
