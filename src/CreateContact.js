85340fabd35fa0f5196ada8ed64cc9ff15dd3897
gaperaltav <gaperaltav@gmail.com>
2018-06-13 20:55:48 -0700

    Creating a CreateContactComponent and handling click event

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+
+    handlerSubmit = (e) => {
+        e.preventDefault();
+        const contact = serializeForm(e.target, { hash: true });
+        
+        if(this.props.onCreateContact)
+            this.props.onCreateContact(contact)
+    }
+
     render() {
         return (
             <div>
-                <h1>Create contact component!</h1>
+                <Link className="close-create-contact" to="/" > go back </Link>
+                <form onSubmit={this.handlerSubmit} className="create-contact-form">
+                    <ImageInput
+                        className="create-contact-avatar-input"
+                        name="avatarUrl"
+                        maxHeight={64}
+                    />
+                    <div className="create-contact-details" >
+                        <input type="text" name="name" placeholder="Name" />
+                        <input type="text" name="email" placeholder="Email" />
+                        <button> Add contact</button>
+                    </div>
+                </form>
             </div>
         )
     }
